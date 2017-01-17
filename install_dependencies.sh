9ad6799b2214f4ac687d418df084d3a0e09261c6
Kishan B <kishancs46@gmail.com>
2017-01-17 13:30:25 +0530

    Installing dependencies via shell script

---
+++ install_dependencies.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -ev
+
+HUGO_VERSION=$1
+
+if [[ ! -s x.txt ]] ; then
+  wget https://github.com/spf13/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz
+  tar -xzf hugo_*.tar.gz
+  mkdir -p tools
+  cp hugo*/hugo* tools/hugo
+  rm -rf hugo_*
+fi
+
+tools/hugo version
+npm install # Temporary workaround since travis does not have yarn out of the box
