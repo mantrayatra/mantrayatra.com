28a3a0325af896d6477185bce4a810ad6e53e451
Kishan B <kishancs46@gmail.com>
2017-01-17 13:34:13 +0530

    My bad

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -3,7 +3,7 @@ set -ev
 
 HUGO_VERSION=$1
 
-if [[ ! -s x.txt ]] ; then
+if [[ ! -s tools/hugo ]] ; then
   wget https://github.com/spf13/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz
   tar -xzf hugo_*.tar.gz
   mkdir -p tools
