203fc52b51450a80cf8f3e91e724dd87ce0357fc
Kishan B <kishancs46@gmail.com>
2018-12-23 23:11:23 +0530

    Removing wget quitness as it should normally be comming from cache

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -4,7 +4,7 @@ set -evx
 HUGO_VERSION=$1
 
 if [[ ! -s tools/hugo ]] ; then
-  wget -q https://github.com/spf13/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz
+  wget https://github.com/spf13/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz
   tar -xzf hugo_*.tar.gz
   mkdir -p tools
   cp hugo tools/
