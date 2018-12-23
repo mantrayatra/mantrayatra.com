526ab01371232b3ccfa4ea0638adc581d62ef377
Kishan B <kishancs46@gmail.com>
2018-12-23 17:11:58 +0530

    Fix hugo binary extraction bug

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-set -ev
+set -evx
 
 HUGO_VERSION=$1
 
 if [[ ! -s tools/hugo ]] ; then
-  wget https://github.com/spf13/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz
+  wget -q https://github.com/spf13/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz
   tar -xzf hugo_*.tar.gz
   mkdir -p tools
-  cp hugo*/hugo* tools/hugo
-  rm -rf hugo_*
+  cp hugo tools/
 fi
 
 tools/hugo version
